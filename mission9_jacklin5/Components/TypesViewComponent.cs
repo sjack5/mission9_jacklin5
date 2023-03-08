48b96496b6b4efcd7db488595dbd9e1833e9b410
Spencer Jacklin <sjacklin01@gmail.com>
2023-03-08 10:46:14 -0700

    Videos 3,4, and 5

---
+++ mission9_jacklin5/Components/TypesViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using mission9_jacklin5.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mission9_jacklin5.Components
+{
+    public class TypesViewComponent : ViewComponent
+    {
+        private IBookstoreRepository repo { get; set; }
+
+        public TypesViewComponent(IBookstoreRepository temp)
+        {
+            repo = temp;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var types = repo.Books
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x);
+
+            return View(types);
+        }
+    }
+}
