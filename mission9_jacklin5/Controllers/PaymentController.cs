fc14e108c9c7d48e312ce9492c61b97cfa3e1644
Spencer Jacklin <sjacklin01@gmail.com>
2023-03-13 21:16:36 -0600

    Video 11

---
+++ mission9_jacklin5/Controllers/PaymentController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using mission9_jacklin5.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mission9_jacklin5.Controllers
+{
+    public class PaymentController : Controller
+    {
+        public IActionResult Checkout()
+        {
+            return View(new Payment());
+        }
+    }
+}
